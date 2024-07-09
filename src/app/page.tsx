6f92264ef0798dfd7e0d1bd3a4b74a9cc784832d
Kim Robinson <kimmykokonut@gmail.com>
2024-07-09 15:04:55 -0700

    fix show form toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,15 +69,14 @@ export default function Home() {
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <h6 className="text-sm italic">{quote} -{author}</h6>
       </div>
-
-      <div className="text-align-left">
-        {!showForm && (
+      <div>
+        <div className="flex justify-center">
           <button
             onClick={() => setShowForm(!showForm)}
             className="group rounded-lg border border-white m-3 px-1 py-1 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-            Enter new job application data
+            {showForm ? 'Exit form' : 'Enter new job application data'}
           </button>
-        )}
+        </div>
         {showForm && <AddApplication />}
       </div>
 
