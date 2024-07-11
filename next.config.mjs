22a6f267dd8885fc189f923694cd8580c90a84b9
Kim Robinson <kimmykokonut@gmail.com>
2024-07-11 11:26:24 -0700

    add yml files for ghpages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/job-journey",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
